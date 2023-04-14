d2c97d52488b8516e82062b74d6657f7a5de0b28
derekpoh <derekpoh_95@hotmail.com>
2023-04-14 15:36:19 +0800

    framwork

---
+++ server.ts
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+const port = process.env.PORT;
+
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "dist")));
+
+
+
+app.get("/api", (req, res) => {
+    res.send("Server working");
+  });
+
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+  });
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
